9ba555e993d369b84c1e391944c22a5a9b147f90
Abhinav Anand <abhinavroy2099@gmail.com>
2025-09-25 21:40:54 +0530

    Add ESLint configuration to ignore warnings during builds

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["res.cloudinary.com"]
-  }
+  },
+  eslint: {
+    // Warning: This will completely ignore ESLint during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
